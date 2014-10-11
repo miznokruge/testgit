5e951f7864216338cf308be5cc8d4c5216930621
mizno <mizno@ptdrbizz.com>
2014-10-11 18:08:26 +0700

    xxx

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
 <?php
 echo 'oke.... jadi gini........';
 ?>
-<br>
-ini hanya proyek jadi2an ya... bukan proyek beneran
-<br>
-<br>
-cuma buat test build berhasil atau nggak
-<br>
-udah gitu aja :p
-<br>
-edited by fahmy
